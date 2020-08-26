28efcf0ff3008f1d0861f337c46dac3497f27783
trongio <giokakabadze50@gmail.com>
2020-08-26 14:17:21 +0400

    Added registration form and backend

---
+++ db/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'signup',
+    'mail_username' =>'user',
+    'mail_password' =>'pass',
+    'mail_host' => 'host',
+    'mail_port' => 587
+];
